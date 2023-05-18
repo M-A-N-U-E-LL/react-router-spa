72d44993aaac36489b77549c3192d80baad835bb
manuelscl <manuelscardenaslopez@gmail.com>
2023-05-18 14:51:46 -0600

    Exhibiendo post y categoría

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Page404 from './pages/Page404'
 import Header from './components/Header'
+import Post from './pages/Post'
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ListCategories from './components/ListCategories'
 
 function App() {
 
@@ -13,9 +15,10 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Sobre' element={<Sobre />}/>
-        <Route path='*' element={<Page404 />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/Sobre' element={<Sobre />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
   );
